bff0ad9f3e0afe46716b205775c24165dbe5d3b4
camilaopazo <camila2bob@hotmail.com>
2014-01-02 11:04:02 -0300

    Create proyecto.c

---
+++ proyecto.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+main(){
+      printf("holii");
+      
+      int a=2;
+      int b=4;
+      int result;
+      
+      result=a+b;
+      printf("result = %d", result);
+      
+      printf("adios");
+      printf("prueba de git diff");
+      
+      getchar();
+      getchar();
+}
